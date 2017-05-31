d48018dca1a50bdee4d5b6cc96601988bccb6644
Yiwanq <752226499@qq.com>
2017-05-31 10:56:50 +0800

    models生成与迁移

---
+++ tiantianshengxian/app_user/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='user',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('uname', models.CharField(max_length=20)),
+                ('upwd', models.CharField(max_length=20)),
+                ('uemail', models.CharField(max_length=20)),
+                ('uphone', models.CharField(max_length=20)),
+                ('uaddress', models.CharField(max_length=50)),
+            ],
+        ),
+    ]
--- tiantianshengxian/app_user/models.py
+++ tiantianshengxian/app_user/models.py
@@ -1,3 +1,10 @@
+# coding: utf-8
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    uname = models.CharField(max_length = 20)
+    upwd = models.CharField(max_length = 20)
+    uemail = models.CharField(max_length = 20)
+    uphone = models.CharField(max_length=20)
+    uaddress = models.CharField(max_length = 50)
