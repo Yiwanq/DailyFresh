55e94eb4d5f7eb999daf75e9fece3117bcec3756
Yiwanq <752226499@qq.com>
2017-06-10 14:03:16 +0800

    初版

--- tiantianshengxian/tiantianshengxian/settings.py
+++ tiantianshengxian/tiantianshengxian/settings.py
@@ -111,6 +111,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = '/var/www/tiantianshengxian/static/'
+              #/var/www/tiantianshengxian/static
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
